44fc9cf58c3e811da26684c09602dc59791b4d8d
Denis Zaynagabdinov <zaiden.ranepa@gmail.com>
2023-07-16 14:46:08 +0300

    initial commit

---
+++ release-3/index.js
@@ -0,0 +1,21 @@
+// {name: 'Lim', surname: 'Bok', age: 12} -> 'Bok Lim'
+export const fullName = () => {
+  // code here
+};
+
+// {key: 5}, 'key2' -> false
+// {key: 5}, 'key' -> true
+// {key: null}, 'key' -> false
+
+export const checkForPropertyAndValue = () => {
+  // code here
+};
+
+// [{name: 'Vova', age: 12, type: 'owner'}, {type: 'computer', owner: 'Vova', name: 'computerName', oc: 'Mac'}, {type: 'phone', name: 'phoneName', owner: 'Vova', oc: 'ios'}] -> [{name: 'Vova', computers: [{ name: 'computerName', oc: 'Mac'}], phones: [{name: 'phoneName', oc: 'ios'}] }]
+export const parser = () => {
+  // code here
+};
+// '{name: 'Vova', age: 12, type: 'owner'}' -> {name: 'Vova', age: 12, type: 'owner'}
+export const createObjFromStr = () => {
+  // code here
+};
